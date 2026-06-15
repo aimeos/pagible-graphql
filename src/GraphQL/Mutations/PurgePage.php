ed1c1dd56774ef961569add5ee4f38cc294a6f8a
aimeos <aimeos@aimeos.org>
2026-06-15 12:07:50 +0000

    New admin build

--- src/GraphQL/Mutations/PurgePage.php
+++ src/GraphQL/Mutations/PurgePage.php
@@ -9,6 +9,8 @@ namespace Aimeos\Cms\GraphQL\Mutations;
 
 use Aimeos\Cms\Models\Page;
 use Aimeos\Cms\Resource;
+use Aimeos\Cms\Utils;
+use Illuminate\Support\Facades\Auth;
 
 
 final class PurgePage
@@ -20,6 +22,6 @@ final class PurgePage
      */
     public function __invoke( $rootValue, array $args ) : array
     {
-        return Resource::purge( Page::class, $args['id'] )->all();
+        return Resource::purge( Page::class, $args['id'], Utils::editor( Auth::user() ) )->all();
     }
 }
