65dc277bd342b26c1cc6fbee5e345e93d84838a1
aimeos <aimeos@aimeos.org>
2026-09-15 07:27:17 +0000

    Implemented support for notifying external systems via webhooks (#313)

--- src/Listeners/UserLogListener.php
+++ src/Listeners/UserLogListener.php
@@ -9,7 +9,6 @@ namespace Aimeos\Cms\Listeners;
 
 use Aimeos\Cms\Events\UserChanged;
 use Aimeos\Cms\Watch;
-use Illuminate\Support\Facades\Log;
 
 
 /**
@@ -25,26 +24,15 @@ class UserLogListener
 {
     public function handle( UserChanged $event ) : void
     {
-        try
-        {
-            $fields = Watch::fields( [
-                'action' => $event->action,
-                'actor' => $event->actorEmail,
-                'target' => $event->targetEmail,
-                'target_id' => $event->targetId,
-                'assignments' => $event->assignments,
-                'ip' => Watch::mask( $event->ip ),
-                'user_agent' => Watch::mask( $event->userAgent ),
-                'tenant_id' => $event->tenant,
-            ] );
-
-            ( $channel = Watch::channel() )
-                ? Log::channel( $channel )->warning( 'cms.user', $fields )
-                : Log::warning( 'cms.user', $fields );
-        }
-        catch( \Throwable $e )
-        {
-            error_log( 'CMS watch listener error: ' . $e->getMessage() );
-        }
+        Watch::warn( 'cms.user', [
+            'action' => $event->action,
+            'actor' => $event->actorEmail,
+            'target' => $event->targetEmail,
+            'target_id' => $event->targetId,
+            'assignments' => $event->assignments,
+            'ip' => Watch::mask( $event->ip ),
+            'user_agent' => Watch::mask( $event->userAgent ),
+            'tenant_id' => $event->tenant,
+        ] );
     }
 }
